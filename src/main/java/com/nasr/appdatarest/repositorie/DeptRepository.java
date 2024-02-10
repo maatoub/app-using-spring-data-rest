184c67c38bd9a9248643c4db75ebb6d9cdf28ba7
maatoub <abdenasser.hasnaoui@edu.uiz.ac.ma>
2024-02-10 15:13:48 +0100

    committed

--- src/main/java/com/nasr/appdatarest/repositorie/DeptRepository.java
+++ src/main/java/com/nasr/appdatarest/repositorie/DeptRepository.java
@@ -1,9 +1,11 @@
 package com.nasr.appdatarest.repositorie;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.nasr.appdatarest.model.Department;
 
+@RepositoryRestResource
 public interface DeptRepository extends JpaRepository<Department, Long> {
 
 }
